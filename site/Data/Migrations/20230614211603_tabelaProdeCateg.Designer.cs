0a5a21126e800daeeefa5a71191cae7c6560d88e
Carlos Legnaro de Almeida <carlos_legnaro@outlook.com>
2023-06-15 17:41:57 -0300

    Criando interfaces para listagem

--- site/Data/Migrations/20230614211603_tabelaProdeCateg.Designer.cs
+++ site/Data/Migrations/20230614211603_tabelaProdeCateg.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace site.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230614211603_tabelaProdeCateg")]
+    partial class tabelaProdeCateg
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
