0f2bb3e41d3d6608013a6c50afc0bdb7f3309b88
Carlos Legnaro de Almeida <carlos_legnaro@outlook.com>
2023-06-16 19:16:52 -0300

    Repositório de Produtos

--- site/Config/ConfigProduto.cs
+++ site/Config/ConfigProduto.cs
@@ -22,14 +22,27 @@ namespace site.Config
                         DescricaoCurta = "Camisa Polo",
                         DescricaoLonga = "Camisa Polo Masculina diversas cores",
                         Categoria = _categogriaRepositorio.Categorias.First(),
-                        ImagemUrl ="",
+                        ImagemUrl ="https://cdn-images.farfetch-contents.com/18/25/78/33/18257833_40996697_600.jpg",
                         EmEstoque = true,
                         Ativo = true,
-                        ImagemMiniaturaUrl =""
+                        ImagemMiniaturaUrl ="https://cdn-images.farfetch-contents.com/18/25/78/33/18257833_40996697_600.jpg"
 
                     },
 
-                    
+                    new Produtos {
+                        Nome = "Saia",
+                        Preco = 200,
+                        DescricaoCurta = "Saia Feminina",
+                        DescricaoLonga = "Saia diversos tamanhos e diversas cores",
+                        Categoria = _categogriaRepositorio.Categorias.Last(),
+                        ImagemUrl ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmki4s5jGwsf3YB0SCLGEqILJuML_40hXJ4NZlyJk5gXumzXwu0iuKjPhl0i5vwqI0cUY&usqp=CAU",
+                        EmEstoque = true,
+                        Ativo = true,
+                        ImagemMiniaturaUrl ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmki4s5jGwsf3YB0SCLGEqILJuML_40hXJ4NZlyJk5gXumzXwu0iuKjPhl0i5vwqI0cUY&usqp=CAU"
+
+                    },
+
+
                 };
             }
         }
