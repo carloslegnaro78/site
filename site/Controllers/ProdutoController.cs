5fd421535030a06180ff3e91cac43892028faaf8
Carlos Legnaro de Almeida <carlos_legnaro@outlook.com>
2023-06-19 14:11:34 -0300

    Criando controller e view

--- site/Controllers/ProdutoController.cs
+++ site/Controllers/ProdutoController.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using site.Config;
+using site.Interfaces;
 
 namespace site.Controllers
 {
     public class ProdutoController : Controller
     {
+        private readonly ICategoriaRepositorio _categoriaRepositorio;
+        private readonly IProdutoRepositorio _produtoRepositorio;
+
         public ProdutoController(ConfigProduto produtoConfig, ConfigCategoria categoriaConfig)
         {
 
+        }
+
+        public ViewResult List()
+        {
+            var produtos = _produtoRepositorio.Produtos;
+
+            return View(produtos);
         }
 
         public IActionResult Index()
