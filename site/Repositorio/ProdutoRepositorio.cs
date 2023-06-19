2fdd9b78b66441e081e94db650fbbe66641a9c58
Carlos Legnaro de Almeida <carlos_legnaro@outlook.com>
2023-06-19 17:14:38 -0300

    Importando viewModels

---
+++ site/Repositorio/ProdutoRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using site.Interfaces;
+using site.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace site.Repositorio
+{
+    public class ProdutoRepositorio : IProdutoRepositorio
+    {
+        public IEnumerable<Produtos> Produtos => throw new NotImplementedException();
+
+        public IEnumerable<Produtos> ProdutosMaisVendidos => throw new NotImplementedException();
+
+        public Produtos GetProdutosById(int produtoId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
