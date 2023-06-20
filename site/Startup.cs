2bfb7fa1a3603136a465393956e49ca3e645359c
Carlos Legnaro de Almeida <carlos_legnaro@outlook.com>
2023-06-20 17:39:16 -0300

    Classe para os repositórios

--- site/Startup.cs
+++ site/Startup.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using site.Config;
 using site.Data;
+using site.Interfaces;
 using site.Models;
 using site.Services;
-using site.Interfaces;
-using site.Config;
 
 namespace site
 {
