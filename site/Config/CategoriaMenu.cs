32b7cbf96f9d39c3791f34fa27111582ac28b24d
Carlos Legnaro de Almeida <carlos_legnaro@outlook.com>
2023-06-29 16:00:20 -0300

    Menu Dropdown para categorias

---
+++ site/Config/CategoriaMenu.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using site.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace site.Config
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepositorio _categoriaRep;
+        
+        public CategoriaMenu(ICategoriaRepositorio repcat)
+        {
+            _categoriaRep = repcat;
+        }
+
+        public IViewComponentResult Inoke()
+        {
+            var categorias = _categoriaRep.Categorias.OrderBy(p => p.NomeCateogira);
+            return View(categorias);
+        }
+
+    }
+}
