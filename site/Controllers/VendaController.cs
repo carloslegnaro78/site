b199831e62819b0d51b5ca8bd91fbe68b6e50d80
Carlos Legnaro de Almeida <carlos_legnaro@outlook.com>
2023-06-30 16:31:27 -0300

    Repositório e interface vendas

---
+++ site/Controllers/VendaController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using site.Interfaces;
+using site.Models;
+
+namespace site.Controllers
+{
+
+
+
+
+
+    [Produces("application/json")]
+    [Route("api/Venda")]
+    public class VendaController : Controller
+    {
+        private readonly IVendaRepositorio _vendaRep;
+        private readonly Carrinho _carrinho;
+
+        public VendaController(IVendaRepositorio vendaRep, Carrinho carrinho)
+        {
+            _vendaRep = vendaRep;
+            _carrinho = carrinho;
+        }
+
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Checkout(Venda venda)
+        {
+            var itens = _carrinho.GetCarrinhoItens();
+            _carrinho.CarrinhoItens = itens;
+            if(_carrinho.CarrinhoItens.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho está vazio, adicione um produto");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _vendaRep.CriarVenda(venda);
+                _carrinho.LimparCarrinho();
+                return RedirectToAction("CheckoutComplete");
+            }
+
+            return View(venda);
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Obrigado pela compra! :)";
+            return View();
+        }
+    }
+}
